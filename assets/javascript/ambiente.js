fc54d6d64f33888715230715921c835f8539689e
ltombely <144056551+ltombely@users.noreply.github.com>
2023-09-15 11:37:00 -0300

    Update JavaScript

---
+++ assets/javascript/ambiente.js
@@ -0,0 +1,3 @@
+const dataAtual = new Date();
+const milissegundosDesde1970 = dataAtual.getTime();
+console.log(milissegundosDesde1970);
--- assets/pages/ambiente.js
+++
@@ -1,2 +0,0 @@
-let media = 5.99999999999999999
-media >= 6? console.log('aprovado'):console.log('reprovado')

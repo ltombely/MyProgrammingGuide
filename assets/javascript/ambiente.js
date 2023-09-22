184b440e5ed4a3ab29d0716fec5400dae3e56d37
ltombely <144056551+ltombely@users.noreply.github.com>
2023-09-22 10:40:18 -0300

    Update

--- assets/javascript/ambiente.js
+++ assets/javascript/ambiente.js
@@ -1,32 +1,12 @@
+let add_btn = document.querySelector('#add_btn')
+add_btn.addEventListener('click', adicionar)
 let array = []
-let todoList = document.querySelector('#todo-list')
 
-function add(){
-    let text = document.querySelector('#text').value
-    let date = document.querySelector('#date').value
+function adicionar(){
 
-    let object = {
-        task: text,
-        time: date
-    }
-
-    object.time = object.time.replace(/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/, '$3/$2/$1' )
-    array.push(object)
-
-    showList()
-
-        
-}
-
-function showList(){
-
-
-    
-}
-
-
-function toDelete(param){
-
-
-
+let txt = document.querySelector('#txt').value
+let date = document.querySelector('#date').value
+array.push(txt)
+console.log(array)
+txt = ''
 }
